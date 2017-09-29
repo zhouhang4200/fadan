76c70c9dfa175ead1c87859092f32c8414453891
zhouhang <zhouhang4200@163.com>
2017-09-29 12:02:59 +0800

    审核表

---
+++ database/migrations/2017_09_29_114921_create_real_name_idents_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRealNameIdentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_name_idents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->comment('用户id');
+            $table->string('license_name')->comment('营业执照名称');
+            $table->string('license_number')->comment('营业执照号码');
+            $table->string('corporation')->comment('法人代表');
+            $table->string('identity_card')->comment('身份证');
+            $table->string('phone_number')->comment('电话号码');
+            $table->string('license_image')->comment('执照照片');
+            $table->string('front_card_picture')->comment('身份证前照');
+            $table->string('back_card_picture')->comment('身份证后照');
+            $table->string('hold_card_picture')->comment('身份证手持照');
+            $table->string('message')->nullable()->comment('审核不通过信息');
+            $table->tinyInteger('status')->unsigned()->default(0)->comment('审核状态,0：审核中，1审核通过，2审核不通过');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_name_idents');
+    }
+}
