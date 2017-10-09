11080bbb8a44ec0813d8b7dfa7a695d99fe8279c
zhouhang <zhouhang4200@163.com>
2017-10-10 01:09:41 +0800

    改路由样式

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,6 @@
 |
 */
 
- // Authentication Routes...
-Route::get('login', 'Frontend\Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Frontend\Auth\LoginController@login');
-Route::post('logout', 'Frontend\Auth\LoginController@logout')->name('logout');
-
-// Registration Routes...
-Route::get('register', 'Frontend\Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Frontend\Auth\RegisterController@register');
-
-// Password Reset Routes...
-Route::get('password/reset', 'Frontend\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Frontend\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Frontend\Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Frontend\Auth\ResetPasswordController@reset');
-
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/home', 'HomeController@index')->name('home');
 });
@@ -43,4 +28,21 @@ Route::middleware(['auth'])->group(function () {
 
 		Route::get('realname/audit', 'RealNameIdent@showAudit')->name('realname.audit');
 	});
+});
+
+Route::namespace('Frontend\Auth')->group(function () {
+	 // 登录
+	Route::get('login', 'LoginController@showLoginForm')->name('login');
+	Route::post('login', 'LoginController@login');
+	Route::post('logout', 'LoginController@logout')->name('logout');
+
+	// 注册
+	Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+	Route::post('register', 'RegisterController@register');
+
+	// 密码找回
+	Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', 'ResetPasswordController@reset');
 });
