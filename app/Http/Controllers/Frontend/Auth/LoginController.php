c048a389f6cdc03b05d1a2f9b3c6e754fe546c0f
zhouhang <zhouhang4200@163.com>
2017-10-10 01:02:32 +0800

    登出跳转为login地址

--- app/Http/Controllers/Frontend/Auth/LoginController.php
+++ app/Http/Controllers/Frontend/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -46,8 +47,28 @@ class LoginController extends Controller
         return 'name';
     }
 
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function showLoginForm()
     {
         return view('frontend.auth.login');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('login');
     }
 }
