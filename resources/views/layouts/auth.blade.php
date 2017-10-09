2450bf42d778ec8ea20ae828b650e5262353ea1e
zhouhang <zhouhang4200@163.com>
2017-10-10 05:16:49 +0800

    修改路径

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>千手 · 订单集市 @yield('title')</title>
-    <link rel="stylesheet" href="vendor/layui/css/layui.css">
-    <link rel="stylesheet" href="frontend/css/login.css">
+    <link rel="stylesheet" href="/vendor/layui/css/layui.css">
+    <link rel="stylesheet" href="/frontend/css/login.css">
     @yield('css')
 </head>
 <body>
