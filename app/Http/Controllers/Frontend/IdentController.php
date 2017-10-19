45181aea46b0821061782d4987b2d42b52ec213a
zhouhang <zhouhang4200@163.com>
2017-10-19 15:14:33 +0800

    审核页面

--- app/Http/Controllers/Frontend/IdentController.php
+++ app/Http/Controllers/Frontend/IdentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Auth;
+use Redis;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -35,7 +37,34 @@ class IdentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userId = Auth()->user()->parent_id ?: Auth()->id();
+
+        $this->validate($request, RealNameIdent::rules(), RealNameIdent::messages());
+
+        if (Redis::get("real:name:ident:phone:$request->phone") !== $request->code) {
+            return back()->withInput()->with('codeError', '验证码错误!');
+        }
+
+        if (Redis::get("real:name:ident:user:$userId") > 5) {
+            return back()->withInput()->with('codeError', '超过当天发送最大次数，请明天再注册!');
+        }
+
+        $data                       = $request->all();
+        $data['user_id']            = $userId;
+        $data['license_number']     = $request->license_number;
+        $data['corporation']        = $request->corporation;
+        $data['identity_card']      = $request->identity_card;
+        $data['phone_number']       = $request->phone_number;
+        $data['license_picture']    = $request->license_picture;
+        $data['front_card_picture'] = $request->front_card_picture;
+        $data['back_card_picture']  = $request->back_card_picture;
+        $data['hold_card_picture']  = $request->hold_card_picture;
+
+        if ($realNameIdent = RealNameIdent::create($data)) {
+
+            return redirect('home');
+        }
+        return back()->withInput()->with('identError', '注册失败！');
     }
 
     /**
@@ -81,5 +110,48 @@ class IdentController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * 点击图片 ajax 上传
+     * @param  Illuminate\Http\Request
+     * @return json
+     */
+    public function uploadImages(Request $request)
+    {
+        if ($request->hasFile($request->name)) {
+
+            $file = $request->file($request->name);
+
+            $path = public_path("/resources/ident/".date('Ymd')."/");
+
+            if ($request->name == 'license_picture') {
+
+                $license = $this->uploadImage($file, $path);
+
+                return json_encode($license);
+            }
+
+            if ($request->name == 'front_card_picture') {
+
+                $front = $this->uploadImage($file, $path);
+
+                return json_encode($front);
+            }
+
+            if ($request->name == 'back_card_picture') {
+
+                $back = $this->uploadImage($file, $path);
+
+                return json_encode($back);
+            }
+
+            if ($request->name == 'hold_card_picture') {
+
+                $hold = $this->uploadImage($file, $path);
+
+                return json_encode($hold);
+            }
+        }
     }
 }
--- resources/views/frontend/user/ident/create.blade.php
+++ resources/views/frontend/user/ident/create.blade.php
@@ -46,7 +46,7 @@
                     </label>
                     <input type="text" class='none' name="license_imagepath">
                     <div class="left h5-exp">示例：</div>
-                    <div class="left h5-upload-file cm-bac" style="background-image:url(/resource/img/license.jpg)"></div>
+                    <div class="left h5-upload-file cm-bac" style="background-image:url(/resources/img/license.jpg)"></div>
                 </div>
             </div>
 
@@ -148,6 +148,100 @@
 <!--START 底部-->
 @section('js')
     <script>
+        layui.use(['form','upload'], function(){
+  var form = layui.form()
+  ,layer = layui.layer;
+ 
+  //自定义验证规则
+  form.verify({
+    code: function(value){
+      if(value.length < 1){
+        return '请填写验证码';
+      }
+    }
+    ,pass: [/(.+){6,12}$/, '密码必须6到12位']
+    ,content: function(value){
+      layedit.sync(editIndex);
+    }
+  });
+  
+    //上传照片
+    var loading;
+    $.each($('.layui-upload-file'), function(index, val) {
+        layui.upload({
+            elem:{"dom":val.id,"token":"{{ csrf_token() }}","name":"qq"},
+            url: '/v1/business/image',
+            method: 'post',
+            before:function(){
+                loading = layer.load(1, {
+                  shade: [0.1,'#000'] //0.1透明度的白色背景
+                });
+            },
+            success: function(res){
+                // console.log(res);
+                layer.closeAll();
+                // console.log(res); //上传成功返回值，必须为json格式
+                $('#'+val.id).parents("label").css('background-image','url('+res+')'); //预览图
+                $("input[name='"+val.id+"path']").val(res); //填充图片路径
+            }
+        });
+    });
+  
+  // 获取验证码
+    var lock=true;
+    $('.get-code').on('click',function(e){
+        e.stopPropagation();
 
+        if(!lock||!$('.phone').val()) return;
+        $.post('', {'id':,'tel':$('.phone').val() }, function(data) {
+            console.log(data);
+            lock=false;
+            var count=60;
+            $('.get-code').html(60+'s');
+            var timer=setInterval(function(){
+                count--;
+                if(count==0){
+                    $('.get-code').html('获取');
+                    clearInterval(timer);
+                    lock=true;
+                }else{
+                    $('.get-code').html(count+'s');
+                }
+            },1000)
+        });
+    });
+  
+  // 监听提交
+  form.on('submit(sub)', function(data){
+    // layer.alert(JSON.stringify(data.field), {
+    //   title: '最终的提交信息'
+    // })
+    // return false;
+  });
+  
+  // 查看协议
+  $('#xy').on('click',function(){
+     layer.open({
+        type: 1
+        ,title:''
+        ,area: ['800px', '80%']
+        ,content: $('ul.xy-list')
+        // ,shade: 0 //不显示遮罩
+        ,yes: function(){
+          layer.closeAll();
+        }
+      });
+  })
+
+  form.on('checkbox(xy)', function(data){
+      if(data.elem.checked){
+        $("button#submit").removeClass('layui-btn-disabled').prop('disabled','');
+      }else{
+        $("button#submit").addClass('layui-btn-disabled').prop('disabled', 'disabled');
+      }; 
+  });        
+      
+  
+});
     </script>
 @endsection
