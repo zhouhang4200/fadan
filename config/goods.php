3ba4e7f71221913752afff51998cd9c9fd173662
Leal <442962403@qq.com>
2017-10-25 14:33:22 +0800

    重命名config文件

---
+++ config/goods.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'template' => [
+        // 存入表中字段字名
+        'field_name' => [
+            'account' => '账号',
+            'password' => '密码',
+            'version' => '版本',
+            'region' => '游戏区',
+            'serve' => '服务器',
+            'role' => '角色',
+            'quantity' => '数量',
+            'foreign_order_no' => '外部订单号',
+        ],
+        // 字段类型
+        'field_type' => [
+            '1' => '输入框',
+            '2' => '下拉项',
+            '3' => '单选项',
+        ],
+    ]
+];
