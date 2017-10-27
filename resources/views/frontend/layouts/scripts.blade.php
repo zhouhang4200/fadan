5461c09db5701a3f975cc38e8a870b36d47e2e9d
zhouhang <zhouhang4200@163.com>
2017-10-27 14:59:32 +0800

    调整前台权限位置

--- resources/views/frontend/layouts/scripts.blade.php
+++ resources/views/frontend/layouts/scripts.blade.php
@@ -4,7 +4,7 @@
 <script>
 $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')}});
 
-var socket = io('http://s.market.dev');
+var socket = io('http://www.market.com:9090');
 
 function logout() {
     layui.use(['form', 'layedit', 'laydate'], function(){
