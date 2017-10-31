f8bfac88ac442b6bac1ebf2320fc02e439ad9e55
zhouhang <zhouhang4200@163.com>
2017-10-31 16:47:47 +0800

    后台resource路由换成普通路由

--- routes/backend.php
+++ routes/backend.php
@@ -106,21 +106,71 @@ Route::middleware(['auth:admin'])->namespace('Backend')->group(function () {
 
     Route::namespace('Rbac')->prefix('rbac')->group(function () {
         // 前台角色
-        Route::resource('roles', 'RoleController', ['except' => ['show']]);
+        // Route::resource('roles', 'RoleController', ['except' => ['show']]);
+        Route::get('roles', 'RoleController@index')->name('roles.index');
+        Route::get('roles/create', 'RoleController@create')->name('roles.create');
+        Route::post('roles', 'RoleController@store')->name('roles.store');
+        Route::get('roles/{id}/edit', 'RoleController@edit')->name('roles.edit');
+        Route::put('roles/{id}', 'RoleController@update')->name('roles.update');
+        Route::delete('roles/{id}', 'RoleController@destroy')->name('roles.destroy');
         // 后台角色
-        Route::resource('admin-roles', 'AdminRoleController', ['except' => ['show']]);
+        // Route::resource('admin-roles', 'AdminRoleController', ['except' => ['show']]);
+        Route::get('admin-roles', 'AdminRoleController@index')->name('admin-roles.index');
+        Route::get('admin-roles/create', 'AdminRoleController@create')->name('admin-roles.create');
+        Route::post('admin-roles', 'AdminRoleController@store')->name('admin-roles.store');
+        Route::get('admin-roles/{id}/edit', 'AdminRoleController@edit')->name('admin-roles.edit');
+        Route::put('admin-roles/{id}', 'AdminRoleController@update')->name('admin-roles.update');
+        Route::delete('admin-roles/{id}', 'AdminRoleController@destroy')->name('admin-roles.destroy');
         // 前台权限
-        Route::resource('permissions', 'PermissionController', ['except' => ['show']]);
+        // Route::resource('permissions', 'PermissionController', ['except' => ['show']]);
+        Route::get('permissions', 'PermissionController@index')->name('permissions.index');
+        Route::get('permissions/create', 'PermissionController@create')->name('permissions.create');
+        Route::post('permissions', 'PermissionController@store')->name('permissions.store');
+        Route::get('permissions/{id}/edit', 'PermissionController@edit')->name('permissions.edit');
+        Route::put('permissions/{id}', 'PermissionController@update')->name('permissions.update');
+        Route::delete('permissions/{id}', 'PermissionController@destroy')->name('permissions.destroy');
         // 后台权限
-        Route::resource('admin-permissions', 'AdminPermissionController', ['except' => ['show']]);
+        // Route::resource('admin-permissions', 'AdminPermissionController', ['except' => ['show']]);
+        Route::get('admin-permissions', 'AdminPermissionController@index')->name('admin-permissions.index');
+        Route::get('admin-permissions/create', 'AdminPermissionController@create')->name('admin-permissions.create');
+        Route::post('admin-permissions', 'AdminPermissionController@store')->name('admin-permissions.store');
+        Route::get('admin-permissions/{id}/edit', 'AdminPermissionController@edit')->name('admin-permissions.edit');
+        Route::put('admin-permissions/{id}', 'AdminPermissionController@update')->name('admin-permissions.update');
+        Route::delete('admin-permissions/{id}', 'AdminPermissionController@destroy')->name('admin-permissions.destroy');
         // 前台账号分配角色
-        Route::resource('groups', 'GroupController');
+        // Route::resource('groups', 'GroupController');
+        Route::get('groups', 'GroupController@index')->name('groups.index');
+        Route::get('groups/create', 'GroupController@create')->name('groups.create');
+        Route::post('groups', 'GroupController@store')->name('groups.store');
+        Route::get('groups/{id}/edit', 'GroupController@edit')->name('groups.edit');
+        Route::get('groups/{id}', 'GroupController@show')->name('groups.show');
+        Route::put('groups/{id}', 'GroupController@update')->name('groups.update');
+        Route::delete('groups/{id}', 'GroupController@destroy')->name('groups.destroy');
         // 后台账号分配角色
-        Route::resource('admin-groups', 'AdminGroupController');
+        // Route::resource('admin-groups', 'AdminGroupController');
+        Route::get('admin-groups', 'AdminGroupController@index')->name('admin-groups.index');
+        Route::get('admin-groups/create', 'AdminGroupController@create')->name('admin-groups.create');
+        Route::post('admin-groups', 'AdminGroupController@store')->name('admin-groups.store');
+        Route::get('admin-groups/{id}/edit', 'AdminGroupController@edit')->name('admin-groups.edit');
+        Route::get('admin-groups/{id}', 'AdminGroupController@show')->name('admin-groups.show');
+        Route::put('admin-groups/{id}', 'AdminGroupController@update')->name('admin-groups.update');
+        Route::delete('admin-groups/{id}', 'AdminGroupController@destroy')->name('admin-groups.destroy');
         // 前台模块
-        Route::resource('modules', 'ModuleController', ['except' => ['show']]);
+        // Route::resource('modules', 'ModuleController', ['except' => ['show']]);
+        Route::get('modules', 'ModuleController@index')->name('modules.index');
+        Route::get('modules/create', 'ModuleController@create')->name('modules.create');
+        Route::post('modules', 'ModuleController@store')->name('modules.store');
+        Route::get('modules/{id}/edit', 'ModuleController@edit')->name('modules.edit');
+        Route::put('modules/{id}', 'ModuleController@update')->name('modules.update');
+        Route::delete('modules/{id}', 'ModuleController@destroy')->name('modules.destroy');
         // 后台模块
-        Route::resource('admin-modules', 'AdminModuleController', ['except' => ['show']]);
+        // Route::resource('admin-modules', 'AdminModuleController', ['except' => ['show']]);
+        Route::get('admin-modules', 'AdminModuleController@index')->name('admin-modules.index');
+        Route::get('admin-modules/create', 'AdminModuleController@create')->name('admin-modules.create');
+        Route::post('admin-modules', 'AdminModuleController@store')->name('admin-modules.store');
+        Route::get('admin-modules/{id}/edit', 'AdminModuleController@edit')->name('admin-modules.edit');
+        Route::put('admin-modules/{id}', 'AdminModuleController@update')->name('admin-modules.update');
+        Route::delete('admin-modules/{id}', 'AdminModuleController@destroy')->name('admin-modules.destroy');
 
         Route::get('accounts', 'AccountController@index')->name('accounts.index');
         // 后台账号
@@ -135,14 +185,18 @@ Route::middleware(['auth:admin'])->namespace('Backend')->group(function () {
         // 实名认证 - 拒绝
         Route::post('refuse', 'PassOrRefuseController@refuse')->name('pass-or-refuse.refuse');
         // 实名认证
-        Route::resource('admin-idents', 'AdminIdentController', ['only' => ['index', 'show']]);
+        // Route::resource('admin-idents', 'AdminIdentController', ['only' => ['index', 'show']]);
+        Route::get('admin-idents', 'AdminIdentController@index')->name('admin-idents.index');
+        Route::get('admin-idents/{id}', 'AdminIdentController@show')->name('admin-idents.show');
 
     });
 
     // 订单
     Route::group([], function () {
         // 订单列表
-        Route::resource('orders', 'OrderController', ['only' => ['index', 'show']]);
+        // Route::resource('orders', 'OrderController', ['only' => ['index', 'show']]);
+        Route::get('orders', 'OrderController@index')->name('orders.index');
+        Route::get('orders/{id}', 'OrderController@show')->name('orders.show');
     });
 
     // 财务
