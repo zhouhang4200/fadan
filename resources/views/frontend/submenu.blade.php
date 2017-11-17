b49e17076265be7f8461d6bc26a09a12ff6d9ea2
zhouhang <zhouhang4200@163.com>
2017-11-17 14:42:47 +0800

    修改详情

--- resources/views/frontend/submenu.blade.php
+++ resources/views/frontend/submenu.blade.php
@@ -1,6 +1,6 @@
 <ul class="seller_center_left_menu">
     <li class="{{ Route::currentRouteName() == 'frontend.index' ? 'current' : '' }}"><a href="{{ route('frontend.index') }}">首页</a><div class="arrow"></div></li>
-    <li class="{{ Route::currentRouteName() == 'data.index' ? 'current' : '' }}"><a href="{{ route('data.index') }}">日常数据</a><div class="arrow"></div></li>
+    <li class="{{ Route::currentRouteName() == 'data.index' ? 'current' : '' }}"><a href="{{ route('data.index') }}">经营数据</a><div class="arrow"></div></li>
     @can('home-punishes.index')
         <li class="{{ Route::currentRouteName() == 'home-punishes.index' ? 'current' : '' }}"><a href="{{ route('home-punishes.index') }}">违规记录</a><div class="arrow"></div></li>
     @endcan
