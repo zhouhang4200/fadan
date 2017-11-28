47a68ff9de236d33d108499335742bd847d490e3
Leal <442962403@qq.com>
2017-11-28 15:26:16 +0800

    增加增加排序

--- app/Repositories/Backend/UserWithdrawOrderRepository.php
+++ app/Repositories/Backend/UserWithdrawOrderRepository.php
@@ -7,7 +7,7 @@ class UserWithdrawOrderRepository
 {
     public function getList($timeStart, $timeEnd, $userId, $no, $status, $pageSize = 20)
     {
-        $dataList = UserWithdrawOrder::orderBy('id', 'desc')
+        $dataList = UserWithdrawOrder::orderBy('creator_primary_user_id')->orderBy('id')
             ->when(!empty($userId), function ($query) use ($userId) {
                 return $query->where('creator_primary_user_id', $userId);
             })
