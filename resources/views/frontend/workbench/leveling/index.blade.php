e90cf9f775f28d06f1ede4e2a428dde365513fb8
zhouhang <zhouhang4200@163.com>
2017-12-29 12:12:39 +0800

    完成操作

--- resources/views/frontend/workbench/leveling/index.blade.php
+++ resources/views/frontend/workbench/leveling/index.blade.php
@@ -561,7 +561,7 @@
                         reload();
                         layer.close(index);
                     });
-                } else if(data.value == 'delete') {
+                } else if(data.value == 'complete') {
                     layer.confirm('确定完成订单？', {icon: 3, title:'提示'}, function(index){
                         $.post("{{ route('frontend.workbench.leveling.status') }}", {
                             orderNo:orderNo,
