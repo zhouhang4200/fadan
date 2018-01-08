2815ede8075e1e3a55b0ac020f2a6e3e7e9a58b4
huangling <buer2202@163.com>
2018-01-08 10:06:19 +0800

    APP接口完成 1.删除测试代码以及无效的插件源 2.增加插件源

--- app/Http/Controllers/Api/App/TestController.php
+++ app/Http/Controllers/Api/App/TestController.php
@@ -13,21 +13,6 @@ class TestController extends Controller
 {
     public function index(Request $request)
     {
-        $userId    = 123;
-        $orderNo   = '2018010213411100000821';
-        $qsOrderId = $request->params['qs_order_id'];
-        $stockId   = $request->params['stock_id'];
-        $gameGold  = $request->params['game_gold'];
-        $productId = $request->params['product_id'];
-        $bundleId  = $request->params['bundle_id'];
-
-        try {
-            OrderChargeRepository::record($orderNo, $qsOrderId, $stockId, $gameGold, $productId, $bundleId, $userId);
-        }
-        catch (CustomException $e) {
-            return response()->jsonReturn(0, $e->getMessage());
-        }
-
-        return response()->jsonReturn(1);
+        #
     }
 }
