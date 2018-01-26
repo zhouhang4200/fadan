4cce3038126667be1af65252b485272b71fe94a5
Leal <442962403@qq.com>
2018-01-26 11:21:16 +0800

    订单修改增加代练小时验证

--- resources/views/frontend/workbench/leveling/detail.blade.php
+++ resources/views/frontend/workbench/leveling/detail.blade.php
@@ -913,6 +913,11 @@
                 return false;
             }
 
+            if(data.field.game_leveling_hour > 24) {
+                layer.msg('代练小时不能大于24小时');
+                return false;
+            }
+
             $.post('{{ route('frontend.workbench.leveling.update') }}', {data: data.field}, function (result) {
                 if (result.status == 1) {
                     layer.open({
