c5231cc5c68672dfd155c63a88ee4e15f1d65eb5
Leal <442962403@qq.com>
2018-02-07 12:35:09 +0800

    修改 代练留言为每秒获取

--- app/Console/Commands/GetMessage.php
+++ app/Console/Commands/GetMessage.php
@@ -42,49 +42,52 @@ class GetMessage extends Command
      */
     public function handle()
     {
-        // 获取所有需要获取留言的订单
-        foreach (levelingMessageGet() as $orderNo => $data) {
-            $data = json_decode($data);
+        while(1) {
+            // 获取所有需要获取留言的订单
+            foreach (levelingMessageGet() as $orderNo => $data) {
+                $data = json_decode($data);
 
-            if ($data->platform == 91) {
-                // 取留言
-                $messageList = Show91::messageList(['oid' => $data->foreign_order_no]);
-                // 转为数组
-                $messageListArr = json_decode(json_encode($messageList), true);
+                if ($data->platform == 91) {
+                    // 取留言
+                    $messageList = Show91::messageList(['oid' => $data->foreign_order_no]);
+                    // 转为数组
+                    $messageListArr = json_decode(json_encode($messageList), true);
 
-                $ids = [];
-                $messageArr = [];
-                foreach ($messageListArr as $item) {
-                    if (isset($item['id']) && $item['uid'] != config('show91.uid')) {
-                        $ids[] = $item['id'];
-                        $messageArr[] = $item;
+                    $ids = [];
+                    $messageArr = [];
+                    foreach ($messageListArr as $item) {
+                        if (isset($item['id']) && $item['uid'] != config('show91.uid')) {
+                            $ids[] = $item['id'];
+                            $messageArr[] = $item;
+                        }
                     }
-                }
-                // 用ID倒序
-                array_multisort($ids, SORT_DESC, $messageArr);
-                // 本次获取的留言数量
-                $currentCount = count($messageArr);
+                    // 用ID倒序
+                    array_multisort($ids, SORT_DESC, $messageArr);
+                    // 本次获取的留言数量
+                    $currentCount = count($messageArr);
 
-                $addCount = $currentCount - $data->count;
+                    $addCount = $currentCount - $data->count;
 
-                $message = [];
+                    $message = [];
 
-                if ($currentCount != $data->count) {
-                    for ($i = $addCount - 1; $i >= 0; $i--) {
-                        $message[] = [
-                            'user_id' => $data->user_id,
-                            'order_no' => $data->order_no,
-                            'contents' => $messageArr[$i]['mess'],
-                            'date' => $messageArr[$i]['created_on'],
-                        ];
+                    if ($currentCount != $data->count) {
+                        for ($i = $addCount - 1; $i >= 0; $i--) {
+                            $message[] = [
+                                'user_id' => $data->user_id,
+                                'order_no' => $data->order_no,
+                                'contents' => $messageArr[$i]['mess'],
+                                'date' => $messageArr[$i]['created_on'],
+                            ];
+                        }
+                        LevelingMessage::insert($message);
+                        // 更新数量
+                        levelingMessageAdd($data->user_id, $orderNo, $data->foreign_order_no, 91, $currentCount);
+                        // 更新角标
+                        levelingMessageCount($data->user_id, 1, $addCount);
                     }
-                    LevelingMessage::insert($message);
-                    // 更新数量
-                    levelingMessageAdd($data->user_id, $orderNo, $data->foreign_order_no, 91, $currentCount);
-                    // 更新角标
-                    levelingMessageCount($data->user_id, 1, $addCount);
                 }
             }
+            sleep(1);
         }
     }
 }
