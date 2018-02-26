ddcfd905e4c2ed7ad134e5d56860efaca4a93aee
zhouhang <zhouhang4200@163.com>
2018-02-26 10:06:25 +0800

    注释冲突代码

--- app/Http/Controllers/Frontend/Statistic/SmsController.php
+++ app/Http/Controllers/Frontend/Statistic/SmsController.php
@@ -28,8 +28,8 @@ class SmsController extends Controller
         $fullUrl = $request->fullUrl();
 
 
-        $record = SmsSendRecord::select(DB::raw('id, date, user_id, count(1) as count'))
-            ->where('user_id', Auth::user()->getPrimaryUserId())
+        // $record = SmsSendRecord::select(DB::raw('id, date, user_id, count(1) as count'))
+        //     ->where('user_id', Auth::user()->getPrimaryUserId())
 
         $query = SmsSendRecord::select(DB::raw('id, date, user_id, count(1) as count'))->where('user_id', Auth::user()->getPrimaryUserId())
             ->filter(compact('startDate', 'endDate'))
