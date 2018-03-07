b98a177a73370f9a29ed563b0e9dda550c69ed3d
Leal <442962403@qq.com>
2018-03-07 12:27:11 +0800

    增加日志

--- app/Extensions/Order/ForeignOrder/KamenForeignOrder.php
+++ app/Extensions/Order/ForeignOrder/KamenForeignOrder.php
@@ -54,6 +54,8 @@ class KamenForeignOrder extends ForeignOrder
         $siteInfo  = SiteInfo::where('kamen_site_id', $jSitd)->first();
         $price = 0; $totalPrice = 0; $wangWang = ''; $remark = '';
 
+        myLog('km', [$jSitd, $siteInfo, $decodeArray]);
+
         if ($siteInfo && $siteInfo->channel == 3 && isset($decodeArray['CustomerOrderNo']) && $decodeArray['CustomerOrderNo']) {
             $tmallOrderInfo = TmallOrderApi::getOrder($siteInfo->kamen_site_id,  $decodeArray['CustomerOrderNo']);
             $price = $tmallOrderInfo['price'];
