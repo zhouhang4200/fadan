bafab9257acd50166703a67f1fd47db22bee74ca
zhouhang <zhouhang4200@163.com>
2018-03-07 16:47:34 +0800

    撤单退钱给杨帆

--- app/Console/Commands/TestDeleteOrder.php
+++ app/Console/Commands/TestDeleteOrder.php
@@ -38,6 +38,8 @@ class TestDeleteOrder extends Command
      */
     public function handle()
     {
-        DailianFactory::choose('delete')->run('2018030715463800000035', '8559', false);
+        DailianFactory::choose('delete')->run('2018030716061300000332', '8317', false);
+        DailianFactory::choose('delete')->run('2018030714543200000295 ', '8317', false);
+        DailianFactory::choose('delete')->run('2018030619343100000639 ', '8317', false);
     }
 }
