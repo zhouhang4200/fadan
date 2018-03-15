71b73b328f032f718a9dae51431427e022536024
Leal <442962403@qq.com>
2018-03-15 15:26:06 +0800

    增加uplay 账号密码验证

--- app/Console/Commands/Temp.php
+++ app/Console/Commands/Temp.php
@@ -41,19 +41,7 @@ class Temp extends Command
      */
     public function handle()
     {
-
-        try {
-            $filePath = public_path('resources/uploads/order/i4JsH9jj6xXy3seNlP06vJRUXsA5wJF5rq5EMNFs.png');
-            // 获取oss 临时上传凭证
-            $certificate = DailianMama::getTempUploadKey();
-            // 实例化oss 上传文件
-            $ossClient = new OssClient($certificate['AccessKeyId'], $certificate['AccessKeySecret'], substr($certificate['prefix_url'], strlen($certificate['bucket_name']) + 8), false, $certificate['SecurityToken']);
-            $result = $ossClient->putObject($certificate['bucket_name'], $certificate['bucket_path'] . basename($filePath), file_get_contents($filePath));
-
-            dd($result);
-        } catch (OssException $e) {
-            print $e->getMessage() . '1';
-        }
+        echo bcrypt('a739452059');
     }
 
 }
