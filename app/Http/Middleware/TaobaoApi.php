140a947d1cc431901bf0879845b108961ce14451
Leal <442962403@qq.com>
2018-03-28 14:08:15 +0800

    去除 淘宝订单api日志

--- app/Http/Middleware/TaobaoApi.php
+++ app/Http/Middleware/TaobaoApi.php
@@ -21,7 +21,7 @@ class TaobaoApi
 
         if (isset($requestAll['data']) && !empty($requestAll['data'])) {
             $request->data = taobaoAesDecrypt($requestAll['data']);
-            myLog('api-request', ['淘宝data解密', $request->url(), $_SERVER['QUERY_STRING'],  $request->data]);
+//            myLog('api-request', ['淘宝data解密', $request->url(), $_SERVER['QUERY_STRING'],  $request->data]);
         } else {
             return response()->tb(0, '请求格式错误');
         }
