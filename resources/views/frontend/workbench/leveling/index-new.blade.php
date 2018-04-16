01b4980ffff1f828fe89ccf0de649a41ce96e832
Leal <442962403@qq.com>
2018-04-16 10:44:33 +0800

    debug

--- resources/views/frontend/workbench/leveling/index-new.blade.php
+++ resources/views/frontend/workbench/leveling/index-new.blade.php
@@ -263,7 +263,7 @@
                             <div class="table-cell" style="width: 220px">
                                 天猫：<a style="color:#1f93ff" href="{{ route('frontend.workbench.leveling.detail') }}?no={{ $item['no'] }}">{{ $detail['source_order_no'] or $item->no  }}</a> <br/>
                                 @if(isset($detail['third']) && $detail['third'])
-                                    {{ config('parent.platform')[$detail['third']] }}：<a style="color:#1f93ff" href="{{ route('frontend.workbench.leveling.detail') }}?no={{ $item['no'] }}"> {{ $detail['third_order_no'] }} </a>
+                                    {{ config('parent.platform')[$detail['third']]['name'] }}：<a style="color:#1f93ff" href="{{ route('frontend.workbench.leveling.detail') }}?no={{ $item['no'] }}"> {{ $detail['third_order_no'] }} </a>
                                 @endif
                             </div>
                         </td>
