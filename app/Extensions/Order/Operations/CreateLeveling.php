88488c01e23741e1e65a6d4a5fc62ee69a0dcacd
Leal <442962403@qq.com>
2018-04-17 09:51:58 +0800

    删除下单方法注释

--- app/Extensions/Order/Operations/CreateLeveling.php
+++ app/Extensions/Order/Operations/CreateLeveling.php
@@ -255,35 +255,35 @@ class CreateLeveling extends \App\Extensions\Order\Operations\Base\Operation
             $redis = RedisConnect::order();
             $redis->lpush('order:send', json_encode($sendOrder));
 
-//            // 给91下订单
-//            $show91Result = Show91::addOrder($this->order);
-//             // 给代练妈妈下订单
-//            $dailianMamaResult = DailianMama::releaseOrder($this->order);
-//
-//            // 判断各个平台下单成功情况
-//            if ($show91Result['status'] && $dailianMamaResult['status']) {
-//                // 以上屏蔽的额逻辑要改，改为存一个91第三方订单号和一个代练妈妈的第三方订单号，同时存在
-//                OrderDetail::where('order_no', $this->order->no)
-//                    ->where('field_name', 'show91_order_no')
-//                    ->update(['field_value' => $show91Result['order_no'],]);
-//
-//                // 如果成功，将订单写入订单详情表
-//                OrderDetail::where('order_no', $this->order->no)
-//                    ->where('field_name', 'dailianmama_order_no')
-//                    ->update(['field_value' => $dailianMamaResult['order_no'],]);
-//            } elseif ($show91Result['status'] && ! $dailianMamaResult['status']) {
-//                // 以上屏蔽的额逻辑要改，改为存一个91第三方订单号和一个代练妈妈的第三方订单号，同时存在
-//                OrderDetail::where('order_no', $this->order->no)
-//                    ->where('field_name', 'show91_order_no')
-//                    ->update(['field_value' => $show91Result['order_no'],]);
-//            } elseif (! $show91Result['status'] && $dailianMamaResult['status']) {
-//                // 如果成功，将订单写入订单详情表
-//                OrderDetail::where('order_no', $this->order->no)
-//                    ->where('field_name', 'dailianmama_order_no')
-//                    ->update(['field_value' => $dailianMamaResult['order_no'],]);
-//            } elseif (! $show91Result['status'] && ! $dailianMamaResult['status']) {
-//                // throw new DailianException('所有平台下单均失败! '.$show91Result['message'].'; '.$dailianMamaResult['message']);
-//            }
+            // 给91下订单
+            $show91Result = Show91::addOrder($this->order);
+             // 给代练妈妈下订单
+            $dailianMamaResult = DailianMama::releaseOrder($this->order);
+
+            // 判断各个平台下单成功情况
+            if ($show91Result['status'] && $dailianMamaResult['status']) {
+                // 以上屏蔽的额逻辑要改，改为存一个91第三方订单号和一个代练妈妈的第三方订单号，同时存在
+                OrderDetail::where('order_no', $this->order->no)
+                    ->where('field_name', 'show91_order_no')
+                    ->update(['field_value' => $show91Result['order_no'],]);
+
+                // 如果成功，将订单写入订单详情表
+                OrderDetail::where('order_no', $this->order->no)
+                    ->where('field_name', 'dailianmama_order_no')
+                    ->update(['field_value' => $dailianMamaResult['order_no'],]);
+            } elseif ($show91Result['status'] && ! $dailianMamaResult['status']) {
+                // 以上屏蔽的额逻辑要改，改为存一个91第三方订单号和一个代练妈妈的第三方订单号，同时存在
+                OrderDetail::where('order_no', $this->order->no)
+                    ->where('field_name', 'show91_order_no')
+                    ->update(['field_value' => $show91Result['order_no'],]);
+            } elseif (! $show91Result['status'] && $dailianMamaResult['status']) {
+                // 如果成功，将订单写入订单详情表
+                OrderDetail::where('order_no', $this->order->no)
+                    ->where('field_name', 'dailianmama_order_no')
+                    ->update(['field_value' => $dailianMamaResult['order_no'],]);
+            } elseif (! $show91Result['status'] && ! $dailianMamaResult['status']) {
+                // throw new DailianException('所有平台下单均失败! '.$show91Result['message'].'; '.$dailianMamaResult['message']);
+            }
 
             return $this->order;      
         }
