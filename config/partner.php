62719877d3e16ca974e89317257152fce26cd13c
zhouhang <zhouhang4200@163.com>
2018-04-17 13:37:00 +0800

    配置文件里面蚂蚁代练名称修改为中文

--- config/partner.php
+++ config/partner.php
@@ -16,7 +16,7 @@ return [
           'aes_iv' => '1234567891111152',
       ],
       3 => [
-          'name' => 'mayi', // 平台名称
+          'name' => '蚂蚁代练', // 平台名称
           'user_id' => 8083, // 千手用户ID
           'receive' => 'dailian.zuhaowan.com/OpenApi/GateWay/index', // 接收订单地址
           'aes_key' => '45584685d8e4f5e8e4e2685',
