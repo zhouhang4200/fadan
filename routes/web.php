2c509e8ed074fcec5488c947ca4790a87b594e82
Leal <442962403@qq.com>
2018-04-20 13:57:25 +0800

    修改：路由文件

--- routes/web.php
+++ routes/web.php
@@ -143,7 +143,7 @@ Route::middleware(['auth:web'])->namespace('Frontend')->group(function () {
         Route::prefix('sending-assist')->group(function () {
             // 代练要求模板
             Route::prefix('require')->group(function () {
-                Route::get('/', 'SendingAssistController@require')->name('frontend.setting.sending-assist.require')->middleware('frontend.setting.sending-assist.auto-markup');
+                Route::get('/', 'SendingAssistController@require')->name('frontend.setting.sending-assist.require');
                 Route::get('create', 'SendingAssistController@requireCreate')->name('frontend.setting.sending-assist.require.create');
                 Route::post('store', 'SendingAssistController@requireStore')->name('frontend.setting.sending-assist.require.store');
                 Route::get('edit/{id}', 'SendingAssistController@requireEdit')->name('frontend.setting.sending-assist.require.edit');
@@ -156,7 +156,7 @@ Route::middleware(['auth:web'])->namespace('Frontend')->group(function () {
             });
             // 自动加价配置
             Route::prefix('auto-markup')->group(function () {
-                Route::get('', 'SendingAssistController@autoMarkup')->name('frontend.setting.sending-assist.auto-markup');
+                Route::get('', 'SendingAssistController@autoMarkup')->name('frontend.setting.sending-assist.auto-markup')->middleware('new.permission:frontend.setting.sending-assist.auto-markup');
                 Route::get('create', 'SendingAssistController@autoMarkupCreate')->name('frontend.setting.sending-assist.auto-markup.create');
                 Route::post('store', 'SendingAssistController@autoMarkupStore')->name('frontend.setting.sending-assist.auto-markup.store');
                 Route::get('edit/{id}', 'SendingAssistController@autoMarkupEdit')->name('frontend.setting.sending-assist.auto-markup.edit');
