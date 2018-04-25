3e6bb16248f832e9b66e2f0ac3c06871ed617a41
Leal <442962403@qq.com>
2018-04-25 17:47:45 +0800

    删除首页无用元素

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -16,36 +16,6 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="main-box infographic-box">
-                <i class="fa fa-user red-bg"></i>
-                <span class="headline">Users</span>
-                <span class="value">2.562</span>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="main-box infographic-box">
-                <i class="fa fa-shopping-cart emerald-bg"></i>
-                <span class="headline">Purchases</span>
-                <span class="value">658</span>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="main-box infographic-box">
-                <i class="fa fa-money green-bg"></i>
-                <span class="headline">Income</span>
-                <span class="value">$8.400</span>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="main-box infographic-box">
-                <i class="fa fa-eye yellow-bg"></i>
-                <span class="headline">Monthly Visits</span>
-                <span class="value">12.526</span>
-            </div>
-        </div>
-    </div>
 @endsection
 
 
