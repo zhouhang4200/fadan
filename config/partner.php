d2af59951fe3c44f78945d7d878d3266d68e38be
Leal <442962403@qq.com>
2018-04-26 18:41:11 +0800

    修改配置文件

--- config/partner.php
+++ config/partner.php
@@ -9,13 +9,13 @@ return [
           'aes_key' => '45584685d8e4f5e8e4e2685',
           'aes_iv' => '1234567891111152',
       ],
-//      2 => [
-//          'name' => '代练妈妈', // 平台名称
-//          'user_id' => 123, // 千手用户ID
-//          'receive' => '', // 接收订单地址
-//          'aes_key' => '45584685d8e4f5e8e4e2685',
-//          'aes_iv' => '1234567891111152',
-//      ],
+      2 => [
+          'name' => '代练妈妈', // 平台名称
+          'user_id' => 123, // 千手用户ID
+          'receive' => '', // 接收订单地址
+          'aes_key' => '45584685d8e4f5e8e4e2685',
+          'aes_iv' => '1234567891111152',
+      ],
       3 => [
           'name' => '蚂蚁代练', // 平台名称
           'user_id' => 8737, // 千手用户ID
@@ -23,12 +23,12 @@ return [
           'aes_key' => '4l5846ssd8e4f5e8e4e2685',
           'aes_iv' => '1234567891111152',
       ],
-//      4 => [
-//          'name' => 'dd373', // 平台名称
-//          'user_id' => 8098, // 千手用户ID
-//          'receive' => 'http://sdk.dd373.com/DLSdk.html?action=pushOrder&platformSign=9cd4100e4af146f487284bb18f190c59', // 接收订单地址
-//          'aes_key' => '45xd46a5d8e4f5e8e4e268x',
-//          'aes_iv' => '1234567891111152',
-//      ],
+      4 => [
+          'name' => 'dd373', // 平台名称
+          'user_id' => 8098, // 千手用户ID
+          'receive' => '-http://sdk.dd373.com/DLSdk.html?action=pushOrder&platformSign=9cd4100e4af146f487284bb18f190c59', // 接收订单地址
+          'aes_key' => '45xd46a5d8e4f5e8e4e268x',
+          'aes_iv' => '1234567891111152',
+      ],
   ]
 ];
