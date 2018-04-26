fd17b133d6f8b606b3b527a8f4d1101704e4f7fd
Leal <442962403@qq.com>
2018-04-26 21:06:48 +0800

    增加接单异常日志

--- app/Extensions/Dailian/Controllers/Playing.php
+++ app/Extensions/Dailian/Controllers/Playing.php
@@ -72,6 +72,7 @@ class Playing extends DailianAbstract implements DailianInterface
             throw new DailianException($exception->getMessage());
         } catch (Exception $exception) {
             DB::rollBack();
+            myLog('playing-ex', [$exception->getFile(), $exception->getMessage()]);
             throw new DailianException('订单异常');
         }
     	DB::commit();
