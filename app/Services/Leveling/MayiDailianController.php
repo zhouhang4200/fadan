0158683a68e5bc41829caaa3afb0306a70b9c31e
zhouhang <zhouhang4200@163.com>
2018-05-03 20:06:27 +0800

    改正

--- app/Services/Leveling/MayiDailianController.php
+++ app/Services/Leveling/MayiDailianController.php
@@ -571,7 +571,7 @@ class MayiDailianController extends LevelingAbstract implements LevelingInterfac
             myLog('mayi-local-error', ['方法' => '撤单', '原因' => $e->getMessage()]);
 
             OrderDetail::where('order_no', $orderDatas['order_no'])
-                ->where('field_value', $orderDatas['mayi_order_no'])
+                ->where('field_name', 'mayi_order_no')
                 ->update(['field_value' => '']);
 
             throw new DailianException($e->getMessage());
