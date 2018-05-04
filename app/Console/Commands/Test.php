b5bc12f86ec73ee1a78279ad770d02a60b23a6c7
Leal <442962403@qq.com>
2018-05-04 23:19:45 +0800

    增加dd373订单价格与状态查询

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -48,6 +48,20 @@ class Test extends Command
         12 => "玩家超时未付款",
     ];
 
+    protected $dd373 = [
+        1 => "未接单",
+        4 => "代练中",
+        5 => "待验收",
+        6 => "已完成",
+        9 => "已撤消",
+        10 => "已结算",
+        11 => "已锁定",
+        12 => "异常",
+        13 => "仲裁中",
+        14 => "已仲裁",
+
+    ];
+
     /**
      * Execute the console command.
      *
@@ -55,21 +69,22 @@ class Test extends Command
      */
     public function handle()
     {
-        $allOrder = \App\Models\Order::where('service_id', 4)->get();
+
+        $allOrder = \App\Models\Order::where('service_id', 4)>where('gainer_primary_user_id', 8739)->get();
 
         foreach ($allOrder as $item) {
-            $show91OrderNO = OrderDetail::where('order_no', $item->no)->where('field_name', 'show91_order_no')->first();
+            $show91OrderNO = OrderDetail::where('order_no', $item->no)->where('field_name', 'dd373_order_no')->first();
             if (isset($show91OrderNO->field_value) && !empty($show91OrderNO->field_value)) {
-                // 如果91订单状态是接单，调我们自己接单接口，如果不是记录一下他们状态
-                $orderDetail = Show91::orderDetail(['oid' => $show91OrderNO->field_value]);
-                // 91 是待验收
+                // 按接单方取订单详情
+                $orderDetail = DD373Controller::orderDetail(['dd373_order_no' => $show91OrderNO->field_value]);
                 if ($orderDetail['data']) {
-                    myLog('91-show-order-query', [
+                    myLog('dd373-show-order-query', [
+                        '第三方' => $item->no,
                         '我们订单号' => $item->no,
-                        '91订单号' => $show91OrderNO->field_value,
-                        '91状态' => isset($this->show91Status[$orderDetail['data']['order_status']]) ? $this->show91Status[$orderDetail['data']['order_status']] : '',
+                        '第三方订单号' => $show91OrderNO->field_value,
+                        '第三方状态' => isset($this->dd373[$orderDetail['data']['orderStatus']]) ? $this->dd373[$orderDetail['data']['orderStatus']] : '',
                         '我们状态' => config('order.status_leveling')[$item->status],
-                        '91从格' => $orderDetail['data']['price'],
+                        '第三方价格' => $orderDetail['data']['price'],
                         '我们价格' => $item->amount,
                         '价格' => $item->amount == $orderDetail['data']['price'] ? '是' : '否'
                     ]);
@@ -77,4 +92,5 @@ class Test extends Command
             }
         }
     }
+
 }
