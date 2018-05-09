4ff40c5c06868b699ba768e9d612d9d3e2f9b8ba
zhouhang <zhouhang4200@163.com>
2018-05-09 12:15:53 +0800

    更换名称

--- config/partner.php
+++ config/partner.php
@@ -5,21 +5,21 @@ return [
       1 => [
           'name' => '91代练', // 平台名称
           'user_id' => 123, // 千手用户ID
-          'receive' => env('SHOW91_URL'), // 接收订单地址
+          'receive' => env('SHOW91_API_URL'), // 接收订单地址
           'aes_key' => '45584685d8e4f5e8',
           'aes_iv' => '1234567891111152',
       ],
       3 => [
           'name' => '蚂蚁代练', // 平台名称
           'user_id' => 8737, // 千手用户ID
-          'receive' => env('MAYI_URL', 'http://www.mayidailian.com/OpenApi/GateWay/index'), // 接收订单地址
+          'receive' => env('MAYI_API_URL', 'http://www.mayidailian.com/OpenApi/GateWay/index'), // 接收订单地址
           'aes_key' => '4l5846ssd8e4f5e8e4e2685',
           'aes_iv' => '1234567891111152',
       ],
       4 => [
           'name' => 'dd373', // 平台名称
           'user_id' => 8739, // 千手用户ID
-          'receive' => env('DD373_URL', 'http://sdk.dd373.com/DLSdk.html?action=pushOrder&platformSign=9cd4100e4af146f487284bb18f190c59'), // 接收订单地址
+          'receive' => env('DD373_API_URL', 'http://sdk.dd373.com/DLSdk.html?action=pushOrder&platformSign=9cd4100e4af146f487284bb18f190c59'), // 接收订单地址
           'aes_key' => '45xd46a5d8e4f5e8e4e268x',
           'aes_iv' => '1234567891111152',
       ],
