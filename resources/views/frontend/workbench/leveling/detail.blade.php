a8d145a3f6c7364109eefea52b763981c18096ff
Leal <442962403@qq.com>
2018-05-16 14:57:52 +0800

    修改订单详情页面淘宝订单数据

--- resources/views/frontend/workbench/leveling/detail.blade.php
+++ resources/views/frontend/workbench/leveling/detail.blade.php
@@ -624,16 +624,16 @@
                             <div class="layui-col-md8">{{ $taobaoTrade->payment or '' }}</div>
                         </div>
                         <div class="layui-row form-group">
-                            <div class="layui-col-md5 text_right">所在区/服：</div>
-                            <div class="layui-col-md7">{{ $fixedInfo['serve']['value'] or '' }}</div>
+                            <div class="layui-col-md3 text_right">所在区/服：</div>
+                            <div class="layui-col-md8">{{ $fixedInfo['serve']['value'] or '' }}</div>
                         </div>
                         <div class="layui-row form-group">
-                            <div class="layui-col-md5 text_right">角色名称：</div>
-                            <div class="layui-col-md7">{{ $fixedInfo['role']['value'] or '' }}</div>
+                            <div class="layui-col-md3 text_right">角色名称：</div>
+                            <div class="layui-col-md8">{{ $fixedInfo['role']['value'] or '' }}</div>
                         </div>
                         <div class="layui-row form-group">
-                            <div class="layui-col-md5 text_right">买家留言：</div>
-                            <div class="layui-col-md7">{{ $taobaoTrade->buyer_message or '' }}</div>
+                            <div class="layui-col-md3 text_right">买家留言：</div>
+                            <div class="layui-col-md8">{{ $taobaoTrade->buyer_message or '' }}</div>
                         </div>
                         <div class="layui-row form-group">
                             <div class="layui-col-md3 text_right">下单时间：</div>
