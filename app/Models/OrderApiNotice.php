60d0436fdd4983e7328d267ab7fad488adacdb92
zhouhang <zhouhang4200@163.com>
2018-05-28 18:33:09 +0800

    改错

--- app/Models/OrderApiNotice.php
+++ app/Models/OrderApiNotice.php
@@ -66,7 +66,7 @@ class OrderApiNotice extends Model
 			$arr['created_at']       = Carbon::now()->toDateTimeString();
 			$arr['updated_at']       = Carbon::now()->toDateTimeString();
 
-	    	static::updateOrCreate($arr, ['order_no' => $datas['order_no'], 'third' => $third, 'function_name' => $functionName]);
+	    	static::updateOrCreate(['order_no' => $datas['order_no'], 'third' => $third, 'function_name' => $functionName], $arr);
     	}
     }
 }
