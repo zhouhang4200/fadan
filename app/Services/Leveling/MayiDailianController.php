70a89417fcb7305ca7363e55574fadb1a4a6bf0e
zhouhang <zhouhang4200@163.com>
2018-06-04 14:55:30 +0800

    报警改为即时向钉钉发送消息

--- app/Services/Leveling/MayiDailianController.php
+++ app/Services/Leveling/MayiDailianController.php
@@ -85,10 +85,30 @@ class MayiDailianController extends LevelingAbstract implements LevelingInterfac
                         $message = $arrResult['message'] ?? 'mayi接口返回错误';
                         // 记录报警
                         $datas['notice_reason'] = $message;
+                        $datas['operate'] = config('leveling.operate')[$functionName] ?? '无';
                         $name = "order:order-api-notices";
                         $key = $datas['order_no'].'-3-'.$functionName;
                         $value = json_encode(['third' => 3, 'reason' => $message, 'functionName' => $functionName, 'datas' => $datas]);
                         Redis::hSet($name, $key, $value);
+
+                        // 往群里发消息
+                        $client = new Client();
+                        $client->request('POST', 'https://oapi.dingtalk.com/robot/send?access_token=54967c90b771a4b585a26b195a71500a2e974fb9b4c9f955355fe4111324eab8', [
+                            'json' => [
+                                'msgtype' => 'text',
+                                'text' => [
+                                    'content' => '订单（内部单号：'.$datas['order_no']. '）调用【'.config('order.third')[3].'】【'.$datas['operate'].'】接口失败:'.$datas['notice_reason']
+                                ],
+                                'at' => [
+                                    'isAtAll' => false,
+                                    "atMobiles" =>  [
+                                        "18500132452",
+                                        "13437284998",
+                                        "13343450907"
+                                    ]
+                                ]
+                            ]
+                        ]);
 
                         if ($options['method'] != 'dlOrderDel') {
                             throw new DailianException($message);
@@ -142,10 +162,30 @@ class MayiDailianController extends LevelingAbstract implements LevelingInterfac
 
                         // 记录报警
                         $datas['notice_reason'] = $message;
+                        $datas['operate'] = config('leveling.operate')[$functionName] ?? '无';
                         $name = "order:order-api-notices";
                         $key = $datas['order_no'].'-3-'.$functionName;
                         $value = json_encode(['third' => 3, 'reason' => $message, 'functionName' => $functionName, 'datas' => $datas]);
                         Redis::hSet($name, $key, $value);
+
+                         // 往群里发消息
+                        $client = new Client();
+                        $client->request('POST', 'https://oapi.dingtalk.com/robot/send?access_token=54967c90b771a4b585a26b195a71500a2e974fb9b4c9f955355fe4111324eab8', [
+                            'json' => [
+                                'msgtype' => 'text',
+                                'text' => [
+                                    'content' => '订单（内部单号：'.$datas['order_no']. '）调用【'.config('order.third')[3].'】【'.$datas['operate'].'】接口失败:'.$datas['notice_reason']
+                                ],
+                                'at' => [
+                                    'isAtAll' => false,
+                                    "atMobiles" =>  [
+                                        "18500132452",
+                                        "13437284998",
+                                        "13343450907"
+                                    ]
+                                ]
+                            ]
+                        ]);
 
                         if ($options['method'] != 'dlOrderDel') {
                             throw new DailianException($message);
