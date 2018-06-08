5d766d106f9b99811b029271cae8264312c4146f
zhouhang <zhouhang4200@163.com>
2018-06-08 11:44:49 +0800

    正式线数据库third改为正确数据

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -217,33 +217,33 @@ class TestController extends Controller
 
     public function index()
     {
-        $orders = OrderBasicData::where('date', '0000-00-00')->get();
+        // $orders = OrderBasicData::where('date', '0000-00-00')->get();
 
-        foreach ($orders as $key => $order) {
-            $date = Carbon::parse($order->order_created_at)->toDateString();
-            $order->date = $date;
-            $order->save();
-        }
+        // foreach ($orders as $key => $order) {
+        //     $date = Carbon::parse($order->order_created_at)->toDateString();
+        //     $order->date = $date;
+        //     $order->save();
+        // }
 
         $thirdOrders = OrderBasicData::where('third', '0')->get();
 
         foreach ($thirdOrders as $thirdOrder) {
             $model = OrderDetail::where('order_no', $thirdOrder->order_no)->where('field_name', 'third')->first();
-            if ($model->third) {
-                $thirdOrder->third = $model->third;
+            if ($model->field_value) {
+                $thirdOrder->third = $model->field_value;
                 $thirdOrder->save();
             }
         }
 
-        $datas = OrderBasicData::get();
+        // $datas = OrderBasicData::get();
 
-        foreach ($datas as $data) {
-            $ord = OrderModel::where('no', $data->order_no)->first();
-            if ($ord->created_at->toDateTimeString() != $data->order_created_at) {
-                $data->order_created_at = $ord->created_at->toDateTimeString();
-                $data->save();
-            }
-        }
+        // foreach ($datas as $data) {
+        //     $ord = OrderModel::where('no', $data->order_no)->first();
+        //     if ($ord->created_at->toDateTimeString() != $data->order_created_at) {
+        //         $data->order_created_at = $ord->created_at->toDateTimeString();
+        //         $data->save();
+        //     }
+        // }
 dd('OK');
 
 
