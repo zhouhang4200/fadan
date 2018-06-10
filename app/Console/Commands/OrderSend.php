918e0c6d060b545ffe879f04a1178053f21cc738
Leal <442962403@qq.com>
2018-06-10 09:06:30 +0800

    增加订单发送日志

--- app/Console/Commands/OrderSend.php
+++ app/Console/Commands/OrderSend.php
@@ -46,7 +46,6 @@ class OrderSend extends Command
         $this->redis = RedisConnect::order();
 
         while (1) {
-            myLog('send-order-run', ['1', '时间' =>date('Y-m-d H:i:s')]);
             $orderData = $this->redis->lpop('order:send');
 
             if($orderData) {
