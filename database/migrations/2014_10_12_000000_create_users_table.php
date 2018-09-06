6bda103fe33f5ac64b5fc3f01f887e7274808a4a
zhouhang <zhouhang4200@163.com>
2018-09-06 10:00:18 +0800

    user表加新字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->tinyInteger('online')->default(0)->comment('是否在线：0 否 1 是');
             $table->tinyInteger('type')->default(1)->comment('1 接单 2 发单');
+            $table->tinyInteger('leveling_type')->default(2)->comment('1 接单 2 发单');
             $table->string('username')->nullable()->comment('昵称');
             $table->tinyInteger('age')->unsigned()->nullable()->comment('年龄');
             $table->string('wechat')->nullable()->comment('微信');
@@ -32,6 +33,7 @@ class CreateUsersTable extends Migration
             $table->string('api_token')->nullable()->comment('api_token');
             $table->string('app_id')->nullable()->comment('app_id');
             $table->string('app_secret')->nullable()->comment('app_secret');
+            $table->string('remark', 100)->nullable()->comment('备注');
             $table->rememberToken();
             $table->timestamp('deleted_at')->nullable()->comment('软删除字段');
             $table->tinyInteger('status')->default(0)->unsigned()->comment('状态：禁用1， 默认，0');
