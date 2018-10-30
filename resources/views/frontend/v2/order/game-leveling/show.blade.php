4fdda773f905579c7f27575d70fb7d8a9b0d6b0a
Leal <442962403@qq.com>
2018-10-30 18:19:54 +0800

    修改：编辑视图增加接口地址

--- resources/views/frontend/v2/order/game-leveling/show.blade.php
+++ resources/views/frontend/v2/order/game-leveling/show.blade.php
@@ -12,6 +12,21 @@
             order-log-api="{{ route('order.game-leveling.log') }}"
             order-add-day-hour-api="{{ route('order.game-leveling.add-day-hour') }}"
             game-leveling-types-api="{{ route('game-leveling-types') }}"
+            delete-api="{{ route('order.game-leveling.delete') }}"
+            on-sale-api="{{ route('order.game-leveling.on-sale') }}"
+            off-sale-api="{{ route('order.game-leveling.off-sale') }}"
+            apply-consult-api="{{ route('order.game-leveling.apply-consult') }}"
+            cancel-consult-api="{{ route('order.game-leveling.cancel-consult') }}"
+            reject-consult-api="{{ route('order.game-leveling.reject-consult') }}"
+            agree-consult-api="{{ route('order.game-leveling.agree-consult') }}"
+            apply-complain-api="{{ route('order.game-leveling.apply-complain') }}"
+            cancel-complain-api="{{ route('order.game-leveling.cancel-complain') }}"
+            arbitration-api="{{ route('order.game-leveling.arbitration') }}"
+            complete-api="{{ route('order.game-leveling.complete') }}"
+            lock-api="{{ route('order.game-leveling.lock') }}"
+            cancel-lock-api="{{ route('order.game-leveling.cancel-lock') }}"
+            anomaly-api="{{ route('order.game-leveling.anomaly') }}"
+            cancel-anomaly-api="{{ route('order.game-leveling.cancel-anomaly') }}">
             game-region-server-api="{{ route('game-region-server') }}">
     </game-leveling-order-show>
 @endsection
