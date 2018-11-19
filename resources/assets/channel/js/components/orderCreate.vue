60aa18c188332f40d9ebcbcf86f99a746aa2d578
zero <442962403@qq.com>
2018-11-19 18:12:32 +0800

    修改支付跳转

--- resources/assets/channel/js/components/orderCreate.vue
+++ resources/assets/channel/js/components/orderCreate.vue
@@ -296,16 +296,14 @@
                                 WeixinJSBridge.invoke(
                                     'getBrandWCPayRequest', res.content.par,
                                     function(result){
-                                        console.log(result);
-                                        if(result.err_msg == "chooseWXPay:fail" ) {
-                                        // if(result.err_msg == "get_brand_wcpay_request:ok" ) {
+                                        if(result.err_msg == "get_brand_wcpay_request:ok" ) {
                                             // 使用以上方式判断前端返回,微信团队郑重提示：
                                             // res.err_msg将在用户支付成功后返回
                                             // ok，但并不保证它绝对可靠。
                                             this.$router.push({
                                                 name:'paySuccess',
                                                 query:{
-                                                    'trade_no' : res.content.trade_no
+                                                    'trade_no' : '2'
                                                 }
                                             });
                                         }
