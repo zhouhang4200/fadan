5108323cd430ba02bb99f4b8d57e929844b540e6
zero <442962403@qq.com>
2018-11-19 19:05:55 +0800

    修改支付跳转

--- resources/assets/channel/js/components/orderCreate.vue
+++ resources/assets/channel/js/components/orderCreate.vue
@@ -287,7 +287,6 @@
                 this.gameServerOptionsShow = false;
             },
             onSubmitForm(){
-
                 this.$validator.validateAll().then((result) => {
                     if (result) {
                         var currentThis = this;
@@ -302,17 +301,14 @@
                                 WeixinJSBridge.invoke(
                                     'getBrandWCPayRequest', res.content.par,
                                     function(result){
-                                        // if(result.err_msg == "get_brand_wcpay_request:ok" ) {
-                                            // 使用以上方式判断前端返回,微信团队郑重提示：
-                                            // res.err_msg将在用户支付成功后返回
-                                            // ok，但并不保证它绝对可靠。
-                                        currentThis.$router.push({
+                                        if(result.err_msg == "get_brand_wcpay_request:ok" ) {
+                                            currentThis.$router.push({
                                                 name:'paySuccess',
                                                 query:{
-                                                    'trade_no' : '2'
+                                                    'trade_no' : res.content.trade_no
                                                 }
                                             });
-                                        // }
+                                        }
                                     }
                                 );
                             }
