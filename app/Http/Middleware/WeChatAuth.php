c7939c6a3315c10c094fcbe106bb481de34ea3d2
zero <442962403@qq.com>
2018-11-20 11:06:29 +0800

    修改微信中间件,还原session forget

--- app/Http/Middleware/WeChatAuth.php
+++ app/Http/Middleware/WeChatAuth.php
@@ -51,7 +51,7 @@ class WeChatAuth
                 return redirect()->to($this->getTargetUrl($request));
             }
 
-//            $request->session()->forget($sessionKey);
+            $request->session()->forget($sessionKey);
 
             return $officialAccount->oauth->scopes($scopes)->redirect($request->fullUrl());
         }
