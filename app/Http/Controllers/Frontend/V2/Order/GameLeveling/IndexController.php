e37aff940bfaf4539b87d8320d0b461ac5e1fdd7
zhouhang <zhouhang4200@163.com>
2018-12-05 17:01:32 +0800

    修复基础数据表拿不到订单金额问题

--- app/Http/Controllers/Frontend/V2/Order/GameLeveling/IndexController.php
+++ app/Http/Controllers/Frontend/V2/Order/GameLeveling/IndexController.php
@@ -217,7 +217,7 @@ class IndexController extends Controller
             if ($orderBasicData) {
                 $orderBasicData->game_id = $order->game_id;
                 $orderBasicData->game_name = $order->gameLevelingOrderDetail->game_name;
-                $orderBasicData->price = $order->price;
+                $orderBasicData->price = $order->amount;
                 $orderBasicData->security_deposit = $order->security_deposit;
                 $orderBasicData->efficiency_deposit = $order->efficiency_deposit;
                 $orderBasicData->original_price = $order->source_price;
@@ -946,122 +946,122 @@ class IndexController extends Controller
         return response()->ajax(1, '操作成功!');
     }
 
-    /**
-     * 修改订单
-     * @return mixed
-     */
-    public function updateOrder()
-    {
-        $requestData = request('data');
-        DB::beginTransaction();
-        try {
-            $gameLevelingOrder = GameLevelingOrder::where('trade_no', request('trade_no'))->first();
-
-            if (!$gameLevelingOrder) {
-                throw new GameLevelingOrderOperateException('订单不存在!');
-            }
-            $gameLevelingOrderDetail = GameLevelingOrderDetail::where('trade_no', request('trade_no'))->first();
-
-            // 未接单
-            if (in_array($gameLevelingOrder->status, [1, 23])) {
-                $gameLevelingOrder->game_id = $requestData['game_id'];
-                $gameLevelingOrder->repeat = $requestData['repeat'];
-                $gameLevelingOrder->amount = $requestData['amount'];
-                $gameLevelingOrder->security_deposit = $requestData['security_deposit'];
-                $gameLevelingOrder->efficiency_deposit = $requestData['efficiency_deposit'];
-                $gameLevelingOrder->game_region_id = $requestData['game_region_id'];
-                $gameLevelingOrder->game_server_id = $requestData['game_server_id'];
-                $gameLevelingOrder->game_leveling_type_id = $requestData['game_leveling_type_id'];
-                $gameLevelingOrder->day = $requestData['day'];
-                $gameLevelingOrder->hour = $requestData['hour'];
-                $gameLevelingOrder->title = $requestData['title'];
-                $gameLevelingOrder->game_account = $requestData['game_account'];
-                $gameLevelingOrder->game_password = $requestData['game_password'];
-                $gameLevelingOrder->game_role = $requestData['game_role'];
-                $gameLevelingOrder->take_order_password = $requestData['take_order_password'];
-                $gameLevelingOrder->price_increase_step = $requestData['price_increase_step'];
-                $gameLevelingOrder->price_ceiling = $requestData['price_ceiling'];
-                $gameLevelingOrder->save();
-
-                $gameLevelingOrderDetail->game_region_name = $requestData['game_region_name'];
-                $gameLevelingOrderDetail->game_server_name = $requestData['game_server_name'];
-                $gameLevelingOrderDetail->game_leveling_type_name = $requestData['game_leveling_type_name'];
-                $gameLevelingOrderDetail->game_name = $requestData['game_name'];
-                $gameLevelingOrderDetail->explain = $requestData['explain'];
-                $gameLevelingOrderDetail->requirement = $requestData['requirement'];
-                $gameLevelingOrderDetail->save();
-            } elseif (in_array($gameLevelingOrder->status, [13, 14, 17, 18])) {
-                // 加价
-                if ($gameLevelingOrder->amount < $requestData['game_leveling_amount']) {
-                    $gameLevelingOrder->amount = $requestData['game_leveling_amount'];
-                    $gameLevelingOrder->save();
-
-                    call_user_func_array([config('gameleveling.controller')[$gameLevelingOrder->platform_id], config('gameleveling.action')['addAmount']], [$gameLevelingOrder]);
-                } elseif ($gameLevelingOrder->amount > $requestData['game_leveling_amount']) {
-                    return response()->ajax(0, '代练价格只可增加!');
-                }
-
-                // 加时
-                if ($requestData['game_leveling_day'] > $gameLevelingOrder->day || ($requestData['game_leveling_day'] == $gameLevelingOrder->day && $requestData['game_leveling_hour'] > $gameLevelingOrder->hour)) {
-                    $gameLevelingOrder->day = $requestData['game_leveling_day'];
-                    $gameLevelingOrder->hour = $requestData['game_leveling_hour'];
-                    $gameLevelingOrder->save();
-                } else {
-                    return response()->ajax(0, '代练时间只可增加!');
-                }
-
-                // 修改账号密码
-                if ($requestData['password'] != $gameLevelingOrder->game_password) {
-                    $gameLevelingOrder->game_password = $requestData['password'];
-                    $gameLevelingOrder->save();
-
-                    call_user_func_array([config('gameleveling.controller')[$gameLevelingOrder->platform_id], config('gameleveling.action')['modifyGamePassword']], [$gameLevelingOrder]);
-                }
-            }
-
-            // 订单日志
-            $historyData = [
-                'order_no' => $gameLevelingOrder->trade_no,
-                'user_id' => $gameLevelingOrder->user_id,
-                'admin_user_id' => '',
-                'type' => 1,
-                'name' => '修改',
-                'description' => "用户[{$gameLevelingOrder->user_id}]修改了订单",
-                'before' => '',
-                'after' => '',
-                'created_at' => Carbon::now()->toDateTimeString(),
-                'creator_primary_user_id' => $gameLevelingOrder->parent_user_id,
-            ];
-
-            OrderHistory::create($historyData);
-
-            // 是否设置了自动加价
-            $gameLevelingOrder->checkAutoMarkUpPrice();
-
-           /***存在来源订单号（淘宝主订单号）, 写入关联淘宝订单表***/
-            GameLevelingOrder::changeSameOriginOrderSourcePrice($gameLevelingOrder, $requestData);
-
-            // 更新基础表数据
-            $orderBasicData = OrderBasicData::where('order_no', $gameLevelingOrder->trade_no)->first();
-
-            $orderBasicData->game_id = $gameLevelingOrder->game_id;
-            $orderBasicData->game_name = $gameLevelingOrderDetail->game_name;
-            $orderBasicData->price = $gameLevelingOrder->price;
-            $orderBasicData->security_deposit = $gameLevelingOrder->security_deposit;
-            $orderBasicData->efficiency_deposit = $gameLevelingOrder->efficiency_deposit;
-            $orderBasicData->original_price = $gameLevelingOrder->source_price;
-            $orderBasicData->save();
-        } catch (GameLevelingOrderOperateException $e) {
-            DB::rollback();
-            return response()->ajax(0, $e->getMessage());
-        } catch (Exception $exception) {
-            DB::rollBack();
-            return response()->ajax(0, '订单异常!');
-        }
-        DB::commit();
-
-        return response()->ajax(1, '修改成功!');
-    }
+//    /**
+//     * 修改订单
+//     * @return mixed
+//     */
+//    public function updateOrder()
+//    {
+//        $requestData = request('data');
+//        DB::beginTransaction();
+//        try {
+//            $gameLevelingOrder = GameLevelingOrder::where('trade_no', request('trade_no'))->first();
+//
+//            if (!$gameLevelingOrder) {
+//                throw new GameLevelingOrderOperateException('订单不存在!');
+//            }
+//            $gameLevelingOrderDetail = GameLevelingOrderDetail::where('trade_no', request('trade_no'))->first();
+//
+//            // 未接单
+//            if (in_array($gameLevelingOrder->status, [1, 23])) {
+//                $gameLevelingOrder->game_id = $requestData['game_id'];
+//                $gameLevelingOrder->repeat = $requestData['repeat'];
+//                $gameLevelingOrder->amount = $requestData['amount'];
+//                $gameLevelingOrder->security_deposit = $requestData['security_deposit'];
+//                $gameLevelingOrder->efficiency_deposit = $requestData['efficiency_deposit'];
+//                $gameLevelingOrder->game_region_id = $requestData['game_region_id'];
+//                $gameLevelingOrder->game_server_id = $requestData['game_server_id'];
+//                $gameLevelingOrder->game_leveling_type_id = $requestData['game_leveling_type_id'];
+//                $gameLevelingOrder->day = $requestData['day'];
+//                $gameLevelingOrder->hour = $requestData['hour'];
+//                $gameLevelingOrder->title = $requestData['title'];
+//                $gameLevelingOrder->game_account = $requestData['game_account'];
+//                $gameLevelingOrder->game_password = $requestData['game_password'];
+//                $gameLevelingOrder->game_role = $requestData['game_role'];
+//                $gameLevelingOrder->take_order_password = $requestData['take_order_password'];
+//                $gameLevelingOrder->price_increase_step = $requestData['price_increase_step'];
+//                $gameLevelingOrder->price_ceiling = $requestData['price_ceiling'];
+//                $gameLevelingOrder->save();
+//
+//                $gameLevelingOrderDetail->game_region_name = $requestData['game_region_name'];
+//                $gameLevelingOrderDetail->game_server_name = $requestData['game_server_name'];
+//                $gameLevelingOrderDetail->game_leveling_type_name = $requestData['game_leveling_type_name'];
+//                $gameLevelingOrderDetail->game_name = $requestData['game_name'];
+//                $gameLevelingOrderDetail->explain = $requestData['explain'];
+//                $gameLevelingOrderDetail->requirement = $requestData['requirement'];
+//                $gameLevelingOrderDetail->save();
+//            } elseif (in_array($gameLevelingOrder->status, [13, 14, 17, 18])) {
+//                // 加价
+//                if ($gameLevelingOrder->amount < $requestData['game_leveling_amount']) {
+//                    $gameLevelingOrder->amount = $requestData['game_leveling_amount'];
+//                    $gameLevelingOrder->save();
+//
+//                    call_user_func_array([config('gameleveling.controller')[$gameLevelingOrder->platform_id], config('gameleveling.action')['addAmount']], [$gameLevelingOrder]);
+//                } elseif ($gameLevelingOrder->amount > $requestData['game_leveling_amount']) {
+//                    return response()->ajax(0, '代练价格只可增加!');
+//                }
+//
+//                // 加时
+//                if ($requestData['game_leveling_day'] > $gameLevelingOrder->day || ($requestData['game_leveling_day'] == $gameLevelingOrder->day && $requestData['game_leveling_hour'] > $gameLevelingOrder->hour)) {
+//                    $gameLevelingOrder->day = $requestData['game_leveling_day'];
+//                    $gameLevelingOrder->hour = $requestData['game_leveling_hour'];
+//                    $gameLevelingOrder->save();
+//                } else {
+//                    return response()->ajax(0, '代练时间只可增加!');
+//                }
+//
+//                // 修改账号密码
+//                if ($requestData['password'] != $gameLevelingOrder->game_password) {
+//                    $gameLevelingOrder->game_password = $requestData['password'];
+//                    $gameLevelingOrder->save();
+//
+//                    call_user_func_array([config('gameleveling.controller')[$gameLevelingOrder->platform_id], config('gameleveling.action')['modifyGamePassword']], [$gameLevelingOrder]);
+//                }
+//            }
+//
+//            // 订单日志
+//            $historyData = [
+//                'order_no' => $gameLevelingOrder->trade_no,
+//                'user_id' => $gameLevelingOrder->user_id,
+//                'admin_user_id' => '',
+//                'type' => 1,
+//                'name' => '修改',
+//                'description' => "用户[{$gameLevelingOrder->user_id}]修改了订单",
+//                'before' => '',
+//                'after' => '',
+//                'created_at' => Carbon::now()->toDateTimeString(),
+//                'creator_primary_user_id' => $gameLevelingOrder->parent_user_id,
+//            ];
+//
+//            OrderHistory::create($historyData);
+//
+//            // 是否设置了自动加价
+//            $gameLevelingOrder->checkAutoMarkUpPrice();
+//
+//           /***存在来源订单号（淘宝主订单号）, 写入关联淘宝订单表***/
+//            GameLevelingOrder::changeSameOriginOrderSourcePrice($gameLevelingOrder, $requestData);
+//
+//            // 更新基础表数据
+//            $orderBasicData = OrderBasicData::where('order_no', $gameLevelingOrder->trade_no)->first();
+//
+//            $orderBasicData->game_id = $gameLevelingOrder->game_id;
+//            $orderBasicData->game_name = $gameLevelingOrderDetail->game_name;
+//            $orderBasicData->price = $gameLevelingOrder->price;
+//            $orderBasicData->security_deposit = $gameLevelingOrder->security_deposit;
+//            $orderBasicData->efficiency_deposit = $gameLevelingOrder->efficiency_deposit;
+//            $orderBasicData->original_price = $gameLevelingOrder->source_price;
+//            $orderBasicData->save();
+//        } catch (GameLevelingOrderOperateException $e) {
+//            DB::rollback();
+//            return response()->ajax(0, $e->getMessage());
+//        } catch (Exception $exception) {
+//            DB::rollBack();
+//            return response()->ajax(0, '订单异常!');
+//        }
+//        DB::commit();
+//
+//        return response()->ajax(1, '修改成功!');
+//    }
 
     /**
      * 发单用户备注
